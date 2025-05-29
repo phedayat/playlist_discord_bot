e145b899e547f4410e5089a29176f005e5d28307
phedayat <phedayat@ucsd.edu>
2025-05-29 15:39:15 -0500

    Updated to use more modular message responses, move a lot of processing to the helper. Trimmed down heavily

--- src/main.py
+++ src/main.py
@@ -3,24 +3,17 @@ import re
 import sys
 
 from discord.client import Client
-from discord import (
-    Message,
-    Intents,
-    DiscordException,
-)
+from discord import Message, Intents
+
+from time import perf_counter
 
 from spotify_helper import (
-    ShareType,
-    get_album_name,
-    get_track_name,
-    build_track_uri,
     get_tracks_to_add,
-    get_playlist_length,
+    get_shared_asset_name,
     add_tracks_to_playlist,
 )
 
-message_regex_raw = r"[\s\S.]*https://\w+\.spotify.com/(\w+)/(\w+)\?[\s\S.]*"
-message_regex = re.compile(message_regex_raw)
+message_regex = re.compile(r"[\s\S.]*https://\w+\.spotify.com/(\w+)/(\w+)\?[\s\S.]*")
 
 class PlaylistBot(Client):
     def __init__(self, playlist_id: str, playlist_name: str, **kwargs):
@@ -36,30 +29,25 @@ class PlaylistBot(Client):
             res = re.match(message_regex, message.content)
             if res:
                 share_type, asset_id = res.groups()
-                n_tracks = get_playlist_length(self.playlist_id)
-                
-                if tracks_to_add := get_tracks_to_add(self.playlist_id, share_type, asset_id):
-                    try:
-                        track_uris = map(build_track_uri, tracks_to_add)
-                        add_tracks_to_playlist(self.playlist_id, track_uris)
-                        
-                        if share_type == ShareType.TRACK:
-                            track_name = get_track_name(asset_id)
-                            await message.reply(f"Track \"{track_name}\" added to \"{self.playlist_name}\"")
-                        else:
-                            album_name = get_album_name(asset_id)
-                            await message.reply(f"Album \"{album_name}\" added to \"{self.playlist_name}\"")
-                        return
-                    except DiscordException as e:
-                        print(f"Error: {e}")
-                else:
-                    if share_type == ShareType.TRACK:
-                        track_name = get_track_name(asset_id)
-                        await message.reply(f"Track \"{track_name}\" already exists in playlist \"{self.playlist_name}\"")
-                    else:
-                        album_name = get_album_name(asset_id)
-                        await message.reply(f"Album \"{album_name}\" already exists in playlist \"{self.playlist_name}\"")
+                share_type_str = share_type.capitalize()
+
+                asset_name = get_shared_asset_name(share_type, asset_id)
+                if asset_name is None:
+                    await message.reply(f"Unsupported share type: {share_type}")
                     return
+
+                timer_start = perf_counter()
+
+                if tracks_to_add := get_tracks_to_add(self.playlist_id, share_type, asset_id):
+                    add_tracks_to_playlist(self.playlist_id, tracks_to_add)
+                    
+                    await message.reply(f"{share_type_str} \"{asset_name}\" added to \"{self.playlist_name}\"")
+                else:
+                    await message.reply(f"{share_type_str} \"{asset_name}\" already exists in playlist \"{self.playlist_name}\"")
+                
+                timer_end = perf_counter()
+                duration = timer_end-timer_start
+                print(f"Duration: {duration}")
 
 
 if __name__=="__main__":
